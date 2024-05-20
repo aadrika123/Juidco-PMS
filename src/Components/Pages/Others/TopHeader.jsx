323b166555d671b8e872e903404a635c27b83973
Almaash <alamalmaash@gmail.com>
2024-05-20 12:15:24 +0530

    Almaash

--- src/Components/Pages/Others/TopHeader.jsx
+++ src/Components/Pages/Others/TopHeader.jsx
@@ -58,54 +58,92 @@ const TopHeader = (props) => {
   return (
     <>
       {isLoading && <BarLoader />}
-      <div className='bg-white flex flex-row justify-between px-2 sm:px-6 border shadow-sm print:hidden py-3'>
-        <div className='flex items-center md:w-[15rem] justify-between gap-2 sm:gap-4'>
-          <span className='font-semibold text-xl '>{brand_tag}</span>
-          <div onClick={() => {
-            settoggleBar(!toggleBar)
-          }}
-          className='ml-[8rem]'>
-            <span className='cursor-pointer text-gray-700 text-xl' ><BiMenuAltLeft className='text-2xl' /></span>
+      <div className="bg-white flex flex-row justify-between px-2 sm:px-6 border shadow-sm print:hidden py-3">
+        <div className="flex items-center md:w-[15rem] justify-between gap-2 sm:gap-4">
+          <span className="font-semibold text-xl ">{brand_tag}</span>
+          <div
+            onClick={() => {
+              settoggleBar(!toggleBar);
+            }}
+            className="ml-[8rem]"
+          >
+            <span className="cursor-pointer text-gray-700 text-xl">
+              <BiMenuAltLeft className="text-2xl" />
+            </span>
           </div>
         </div>
 
-        <div className=' w-full ml-2 flex items-center'>
-        <span onClick={() => openModal2()} className='bg-gray-200 px-4 py-1 cursor-pointer hover:shadow-md'>Modules</span> <span className='pl-4 font-bold text-xl'>Procurement Management System </span> </div>
-        <div className='flex items-center sm:gap-4 gap-2'>
-          <span className='sm:visible flex items-center '>
-          <Tooltip anchorId="logout" className='z-50' />
-            <button id='logout' data-tooltip-content="Log Out" onClick={() => openModal()} className='text-2xl font-semibold bg-[#4338CA] text-white rounded p-1'><BiLogOutCircle /></button></span>
-          <Tooltip anchorId="notification_icon" className='z-50' />
-          <BsBell id='notification_icon' data-tooltip-content="Show all notifications." onClick={() => setnotificationState(true)} className="cursor-pointer inline text-2xl font-semibold text-[#4338CA]" />
+        <div className=" w-1/3 ml-2 flex items-center">
+          <span
+            onClick={() => openModal2()}
+            className="bg-gray-200 px-4 py-1 cursor-pointer hover:shadow-md"
+          >
+            Modules
+          </span>{" "}
+        </div>
+        
+        <div className="flex items-center sm:gap-4 gap-2">
+          <span className="sm:visible flex items-center ">
+          <h1>Procurement Management System</h1>
+            <Tooltip anchorId="logout" className="z-50" />
+            <button
+              id="logout"
+              data-tooltip-content="Log Out"
+              onClick={() => openModal()}
+              className="text-2xl font-semibold bg-[#4338CA] text-white rounded p-1"
+            >
+              <BiLogOutCircle />
+            </button>
+          </span>
+          <Tooltip anchorId="notification_icon" className="z-50" />
+          <BsBell
+            id="notification_icon"
+            data-tooltip-content="Show all notifications."
+            onClick={() => setnotificationState(true)}
+            className="text-3xl font-semibold bg-[#4338CA] text-white rounded p-1 cursor-pointer"
+          />
         </div>
       </div>
-
 
       {/* ===========MODAL========= */}
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        className='h-screen w-screen flex items-center justify-center'
+        className="h-screen w-screen flex items-center justify-center"
         contentLabel="Logout Modal"
       >
+        <div
+          className="bg-black absolute h-screen w-screen opacity-50 z-0 "
+          onClick={closeModal}
+        ></div>
 
-        <div className='bg-black absolute h-screen w-screen opacity-50 z-0 ' onClick={closeModal}></div>
-
-        <div className='border bg-white z-50 px-6 py-4 flex flex-col gap-4 animate__animated animate__slideInLeft animate__faster'>
-          <div className='flex items-center gap-6'>
-            
-            <div className='flex flex-col gap-2'>
-            <span className='text-red-500  block rounded-full drop-shadow-md shadow-red-300 ml-24'><FiAlertCircle size={25}  /></span>
-              <span className='text-xl font-semibold border-b pb-1 text-center'>Confirmation</span>
-              <span className='text-base'>Are you sure want to log out ?</span>
+        <div className="border bg-white z-50 px-6 py-4 flex flex-col gap-4 animate__animated animate__slideInLeft animate__faster">
+          <div className="flex items-center gap-6">
+            <div className="flex flex-col gap-2">
+              <span className="text-red-500  block rounded-full drop-shadow-md shadow-red-300 ml-24">
+                <FiAlertCircle size={25} />
+              </span>
+              <span className="text-xl font-semibold border-b pb-1 text-center">
+                Confirmation
+              </span>
+              <span className="text-base">Are you sure want to log out ?</span>
             </div>
           </div>
-          <div className='flex justify-end gap-2'>
-            <button className='text-white bg-indigo-500 hover:bg-indigo-600 px-4 py-1 text-sm ' onClick={closeModal}>No</button>
-            <button className='text-white bg-red-500 hover:bg-red-600 px-4 py-1 text-sm ' onClick={LogOutUser}>Yes</button>
+          <div className="flex justify-end gap-2">
+            <button
+              className="text-white bg-indigo-500 hover:bg-indigo-600 px-4 py-1 text-sm "
+              onClick={closeModal}
+            >
+              No
+            </button>
+            <button
+              className="text-white bg-red-500 hover:bg-red-600 px-4 py-1 text-sm "
+              onClick={LogOutUser}
+            >
+              Yes
+            </button>
           </div>
         </div>
-
       </Modal>
 
       <Modal
@@ -115,14 +153,12 @@ const TopHeader = (props) => {
         contentLabel="Example Modal"
       >
         <PermittedModuleCard closeModuleModal={closeModal} />
-
       </Modal>
-      {
-        notificationState && <NotificationComponent setnotificationState={setnotificationState} />
-      }
-
+      {notificationState && (
+        <NotificationComponent setnotificationState={setnotificationState} />
+      )}
     </>
-  )
+  );
 }
 
 // export default HeaderIcons
