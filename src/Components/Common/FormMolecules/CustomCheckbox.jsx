048bdae1d3b219517162f37b51297fbe94b86acc
pandeyprity <pandeyprity33@gmail.com>
2024-05-30 12:47:40 +0530

    checkbox comp

--- src/Components/Common/FormMolecules/CustomCheckbox.jsx
+++ src/Components/Common/FormMolecules/CustomCheckbox.jsx
@@ -1,3 +1,50 @@
-export default function CustomCheckbox() {
-  return <div>checkbox</div>;
-}
+import React from "react";
+import {
+  Box,
+  FormLabel,
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  FormHelperText,
+  Checkbox,
+} from "@mui/material";
+
+const CustomCheckboxGroup = ({
+  fields,
+  values,
+  handleChange,
+  errors,
+  touched,
+  title,
+}) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <FormControl
+        sx={{ m: 3 }}
+        component='fieldset'
+        variant='standard'
+        error={Boolean(errors && touched)}
+      >
+        <FormLabel component='legend'>{title}</FormLabel>
+        <FormGroup>
+          {fields.map((opt) => (
+            <FormControlLabel
+              key={opt.value}
+              control={
+                <Checkbox
+                  //   checked={values[opt.value]}
+                  onChange={handleChange}
+                  name={`checkboxes.${opt.value}`}
+                />
+              }
+              label={opt.label}
+            />
+          ))}
+        </FormGroup>
+        {errors && touched && <FormHelperText>{errors}</FormHelperText>}
+      </FormControl>
+    </Box>
+  );
+};
+
+export default CustomCheckboxGroup;
