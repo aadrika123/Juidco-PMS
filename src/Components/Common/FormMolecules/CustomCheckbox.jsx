886b1e22f297212a6f63d639f7e45bc3deb5512c
pandeyprity <pandeyprity33@gmail.com>
2024-05-30 13:18:16 +0530

    checkbox css

--- src/Components/Common/FormMolecules/CustomCheckbox.jsx
+++ src/Components/Common/FormMolecules/CustomCheckbox.jsx
@@ -20,18 +20,24 @@ const CustomCheckboxGroup = ({
   return (
     <Box sx={{ display: "flex" }}>
       <FormControl
-        sx={{ m: 3 }}
+        sx={{ m: 3, width: "100%" }}
         component='fieldset'
         variant='standard'
         error={Boolean(errors && touched)}
       >
-        <FormLabel component='legend'>{title}</FormLabel>
-        <FormGroup>
+        <FormLabel component='legend' sx={{ color: "black" }}>
+          {title}
+        </FormLabel>
+        <FormGroup
+          className='text-gray-600'
+          sx={{ display: "flex", flexDirection: "row" }}
+        >
           {fields.map((opt) => (
             <FormControlLabel
               key={opt.value}
               control={
                 <Checkbox
+                  size='small'
                   //   checked={values[opt.value]}
                   onChange={handleChange}
                   name={`checkboxes.${opt.value}`}
